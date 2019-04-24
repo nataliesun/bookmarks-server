79ef440ffafa6cd83f927477f17c5fb5e0a06e0c
Kyler <Krenneker16@gmail.com>
2019-04-23 21:10:25 -0400

    added routing and tested for full functionality

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const winston = require('winston')
-const uuid = require('uuid/v4')
+// const winston = require('winston')
+const logger = require('./logger')
 const { NODE_ENV } = require('./config')
+const bookmarkRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
@@ -17,29 +18,6 @@ app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
 app.use(express.json())
-
-const bookmarks = [{
-  id: '1',
-  title: 'Facebook',
-  url: 'www.facebook.com',
-  description: 'social networking',
-  rating: 4
-}]
-
-// set up winston
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'info.log' })
-  ]
-});
-
-if (NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
 
 //validate API
 app.use(function validateBearerToken(req, res, next) {
@@ -54,109 +32,16 @@ app.use(function validateBearerToken(req, res, next) {
   next()
 })
 
-
-app.get('/bookmarks', (req, res) => {
-  res.json(bookmarks)
-})
-
-app.get('/bookmarks/:id', (req, res) => {
-  const { id } = req.params;
-  const bookmark = bookmarks.find(c => c.id == id);
-
-  // make sure we found a card
-  if (!bookmark) {
-    logger.error(`Card with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Card Not Found');
-  }
-
-  res.json(bookmark);
-});
-
-app.post('/bookmarks', (req, res) => {
-  console.log(req.body)
-  const { title, url, description, rating } = req.body;
-
-  if (!title) {
-    logger.error(`Title is required`);
-    return res
-      .status(400)
-      .send('Invalid data 1');
-  }
-  
-  if (!url) {
-    logger.error(`Url is required`);
-    return res
-      .status(400)
-      .send('Invalid data 2');
-  }
-
-  if (!description) {
-    logger.error(`Description is required`);
-    return res
-      .status(400)
-      .send('Invalid data 3');
-  }
-
-  if (!rating) {
-    logger.error(`Rating is required`);
-    return res
-      .status(400)
-      .send('Invalid data 4');
-    }
-
-  const id = uuid();
-
-  const bookmark = {
-    id,
-    title,
-    url,
-    description,
-    rating
-  };
-
-  bookmarks.push(bookmark);
-
-  logger.info(`Bookmark with id ${id} created`);
-
-  res
-    .status(201)
-    .location(`http://localhost:8000/bookmark/${id}`)
-    .json(bookmark);
-  });
-
-
-app.delete('/bookmarks/:id', (req, res) => {
-  const { id } = req.params;
-
-  const bookmarkIndex = bookmarks.findIndex(b => b.id == id);
-
-  if (bookmarkIndex === -1) {
-    logger.error(`Bookmark with id ${id} not found.`);
-    return res
-      .status(404)
-      .send('Not found');
-  }
-
-  bookmarks.splice(bookmarkIndex, 1);
-
-  logger.info(`Bookmark with id ${id} deleted.`);
-
-  res
-    .status(204)
-    .end();
-});
-
+app.use(bookmarkRouter)
 
 app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
 module.exports = app
